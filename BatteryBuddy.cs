b462d2e35f934d2793123cb786b7a6ae8a18beb7
librarianmage <raymond.arndorfer@gmail.com>
2025-10-05 18:27:52 -0500

    Simplify names

--- BatteryBuddy.cs
+++ BatteryBuddy.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using XRL;
 using XRL.UI;
 using XRL.World;
-using XRL.World.Anatomy;
 using XRL.World.Parts;
-using XRL.World.Parts.Skill;
 
 namespace BatteryBuddy
 {
-    [HasCallAfterGameLoadedAttribute]
+    [HasCallAfterGameLoaded]
     public class BatteryBuddy : IPlayerPart
     {
         public static readonly string MOD_ID = "BatteryBuddy";
-        
+
         public override void Register(GameObject Object, IEventRegistrar Registrar)
         {
             Registrar.Register("EquipperUnequipped");
@@ -35,12 +30,12 @@ namespace BatteryBuddy
         {
             return base.WantEvent(ID, cascade) || ID == EquipperEquippedEvent.ID || ID == AfterPlayerBodyChangeEvent.ID;
         }
-        
-        public override bool HandleEvent(XRL.World.EquipperEquippedEvent E)
+
+        public override bool HandleEvent(EquipperEquippedEvent E)
         {
             if (E.Actor.IsPlayer())
             {
-                E.Item.RegisterEvent(this, XRL.World.CellDepletedEvent.ID);
+                E.Item.RegisterEvent(this, CellDepletedEvent.ID);
             }
             return base.HandleEvent(E);
         }
@@ -52,17 +47,17 @@ namespace BatteryBuddy
                 bool usePopup = Options.GetOptionBool("OptionBatteryBuddyPopup");
                 if (XRL.Rules.Stat.RandomCosmetic(0, 99) < 50)
                 {
-                    XRL.World.Capabilities.Messaging.XDidY(E.Cell.SlottedIn, "emit", Extra: "a sad hiss as its energy cell gives up", UsePopup: usePopup, Color: "W");
+                    XDidY(E.Cell.SlottedIn, "emit", Extra: "a sad hiss as its energy cell gives up", UsePopup: usePopup, Color: "W");
                 }
                 else
                 {
-                    XRL.World.Capabilities.Messaging.XDidY(E.Cell.SlottedIn, "sizzle and pop", Extra: "as it runs out of power", UsePopup: usePopup, Color: "W");
+                    XDidY(E.Cell.SlottedIn, "sizzle and pop", Extra: "as it runs out of power", UsePopup: usePopup, Color: "W");
                 }
             }
 
             return base.HandleEvent(E);
         }
-        
+
         public override bool HandleEvent(AfterPlayerBodyChangeEvent E)
         {
             UnregisterEquipment(E.OldBody);
@@ -74,32 +69,32 @@ namespace BatteryBuddy
         {
             GO?.ForeachEquippedObject((GO) => GO.RegisterEvent(this, CellDepletedEvent.ID));
         }
-        
+
         public void UnregisterEquipment(GameObject GO)
         {
             GO?.ForeachEquippedObject((GO) => GO.UnregisterEvent(this, CellDepletedEvent.ID));
         }
-        
-        public void Initialize()
+
+        public void InitializeRegistration()
         {
             // ensure registration for all existing equipment when loading a new save
-            RegisterEquipment(XRL.The.Player);
+            RegisterEquipment(The.Player);
         }
-        
-        [CallAfterGameLoadedAttribute]
+
+        [CallAfterGameLoaded]
         public static void LoadGameCallback()
         {
             // Called whenever loading a save game
-            XRL.The.Player.RequirePart<BatteryBuddy>().Initialize();
+            The.Player.RequirePart<BatteryBuddy>().InitializeRegistration();
         }
 	}
-    
+
     [PlayerMutator]
     public class MyPlayerMutator : IPlayerMutator
     {
         public void mutate(GameObject player)
         {
-            player.RequirePart<BatteryBuddy>().Initialize();
+            player.RequirePart<BatteryBuddy>().InitializeRegistration();
         }
     }
 }
